089d7ea98d965f2f9fc2b8e18fb08419ed1d6a88
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-30 01:38:52 +0530

    Create write_sequence.sv

---
+++ Memory/write_sequence.sv
@@ -0,0 +1,13 @@
+class mem_wr_seq extends uvm_sequence#(mem_seq_item);
+  
+  `uvm_object_utils(mem_wr_seq)
+  
+  function new(string name = "mem_wr_seq");
+    super.new(name);
+  endfunction
+  
+  virtual task body();
+    `uvm_do_with(req,{ req.wr_en == 1; })
+  endtask
+  
+endclass
